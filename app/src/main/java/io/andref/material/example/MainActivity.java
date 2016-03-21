471e45c13aa20fedd3893c0285a5705e46347890
Michael De Soto <michael.desoto@snagajob.com>
2016-03-21 16:54:41 -0400

    Very rough three line list item. Only checking in to test JitPack.

---
+++ app/src/main/java/io/andref/material/example/MainActivity.java
@@ -0,0 +1,15 @@
+package io.andref.material.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.three_line_list_with_avatar_and_icon);
+    }
+}
